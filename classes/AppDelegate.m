2ef36472e24ee388329c4afb73504211b91e378b
Ashok <kashokraju@artisan-engineering.com>
2014-05-06 13:31:31 -0500

    manually resolving merge conflicts

--- classes/AppDelegate.m
+++ classes/AppDelegate.m
@@ -20,8 +20,6 @@
 
 @implementation AppDelegate
 
-@synthesize gtfsAutoUpdater;
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     [[UITabBar appearance] setSelectedImageTintColor:[UIColor whiteColor]];
@@ -42,7 +40,6 @@
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    [gtfsAutoUpdater cancelAutoUpdate];
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
